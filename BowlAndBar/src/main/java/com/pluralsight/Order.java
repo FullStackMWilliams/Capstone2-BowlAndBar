f508fd6abebb713ff08a6ec8048c08a79fb79a72
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-10 16:57:22 -0500

    ᕦ(ò_óˇ) Added attributes for Order class

--- BowlAndBar/src/main/java/com/pluralsight/Order.java
+++ BowlAndBar/src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<BowlItem> items;
+    private List<Drink> drinks;
+    private List<Side> sides;
+
+    public Order() {
+        this.items = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.sides = new ArrayList<>();
+    }
+
 }
