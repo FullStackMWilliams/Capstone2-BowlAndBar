669228e9d73d5c1ef5ec787fbde743bb21d7802b
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-10 15:06:02 -0500

    (っ ˆڡˆ ς) Done with constructors for Drinks class

--- BowlAndBar/src/main/java/com/pluralsight/Drink.java
+++ BowlAndBar/src/main/java/com/pluralsight/Drink.java
@@ -5,4 +5,13 @@ import com.pluralsight.enums.DrinkSize;
 public class Drink {
     private DrinkSize size;
     private String flavor;
+
+    private static final String[] Flavors = {
+            "Water", "Coconut water", "Lemonade", "Iced tea", "Kombucha"
+    };
+
+    public Drink(DrinkSize size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
 }
