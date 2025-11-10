4e9f6822573471532fc14e4dc6f63268cba45fc2
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-10 13:58:38 -0500

    ԅ(≖‸≖ԅ) Done with Smoothie class

--- BowlAndBar/src/main/java/com/pluralsight/Smoothie.java
+++ BowlAndBar/src/main/java/com/pluralsight/Smoothie.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
-public class Smoothie {
+import com.pluralsight.enums.ItemSize;
+
+public class Smoothie extends BowlItem {
+    private static final String[] TYPES =  {"Berry Blast", "Tropical Paradise","Green Machine","Chocolate Peanut"};
+
+    public Smoothie (ItemSize size, String type) {
+        super(size, type);
+    }
+
+    public static String[] getTypes() {
+        return TYPES;
+    }
+
+    @Override
+    public double getPrice() {
+        double price = getBasePrice() + getToppingsPrice();
+        if (specialized) {
+            price += 1.00;
+        }
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Smoothie - " + super.toString();
+    }
 }
