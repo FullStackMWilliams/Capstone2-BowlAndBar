968781a2c0f5c9a4a65cc632d946656ecd08ce30
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-10 14:22:37 -0500

    ᘳ≖ꈊ≖ᘰ setting up constructors and "inheritance" for YogurtBowl class

--- BowlAndBar/src/main/java/com/pluralsight/enums/YogurtBowl.java
+++ BowlAndBar/src/main/java/com/pluralsight/enums/YogurtBowl.java
@@ -3,5 +3,15 @@ package com.pluralsight.enums;
 // this class will help us
 
 
-public class YogurtBowl {
+import com.pluralsight.BowlItem;
+
+public class YogurtBowl extends BowlItem {
+    private static final String[] TYPES = {"Greek Yogurt","Vanilla Yogurt","Coconut Yogurt", "Plain Yogurt"};
+
+    public YogurtBowl(ItemSize size,String type) {
+        super(size, type);
+    }
+    public static String[] getTypes() {
+        return TYPES;
+    }
 }
