4037240f14566fcdb2da3c1d74de10c2d200a5b9
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-10 13:24:53 -0500

    〈≖_≖〝〉 Done with getters/setters and constructors for BowlItem class

--- BowlAndBar/src/main/java/com/pluralsight/BowlItem.java
+++ BowlAndBar/src/main/java/com/pluralsight/BowlItem.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
 
-public class BowlItem {
+import com.pluralsight.enums.ItemSize;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BowlItem {
+    protected ItemSize size;
+    protected String type;
+    protected List<Topping> toppings;
+    protected boolean specialized;
+
+    public BowlItem(ItemSize size, String type) {
+        this.size = size;
+        this.type = type;
+        this.toppings = new ArrayList<>();
+        this.specialized = false;
+    }
+
+    public ItemSize getSize() {
+        return size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    public void setSpecialized(boolean specialized) {
+        this.specialized = specialized;
+    }
+
+    public abstract double getPrice();
+
+    protected double getBasePrice() {
+
+    }
 }
