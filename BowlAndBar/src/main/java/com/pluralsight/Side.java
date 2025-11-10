44d8bd4ef86915a3c506af7dbc700309f3dd18c1
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-10 16:36:29 -0500

    ୧ʕ•̀ ヮ•́ʔ୨ Done with attributes for Side's class

--- BowlAndBar/src/main/java/com/pluralsight/Side.java
+++ BowlAndBar/src/main/java/com/pluralsight/Side.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class Side {
+    private String type;
+    private static final double PRICE = 1.50;
+
+    private static final String[] TYPES = {
+            "Energy Bites", "Protein Bar"
+    };
 }
