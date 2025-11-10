8fd83cc3bc0f5ecc05233d5296e2df1d425e2c96
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-10 18:03:53 -0500

    ฅʕ●//ᴥ//●ʔ Starting writeReceipt method

--- BowlAndBar/src/main/java/com/pluralsight/ReceiptWriter.java
+++ BowlAndBar/src/main/java/com/pluralsight/ReceiptWriter.java
@@ -7,4 +7,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ReceiptWriter {
+
+    private static final String RECEIPTS_FOLDER = "receipts";
+
+    public static void writeReceipt(Order order) {
+        File folder = new File(RECEIPTS_FOLDER);
+    }
 }
