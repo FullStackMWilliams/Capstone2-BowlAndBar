1b9e4cd0961a8657d64d6b4cd40812937ff558aa
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-11 18:51:14 -0500

    (˙灬˙ ) Added main class attributes/objects for customer options

--- BowlAndBar/src/main/java/com/pluralsight/BowlAndBarApp.java
+++ BowlAndBar/src/main/java/com/pluralsight/BowlAndBarApp.java
@@ -1,8 +1,33 @@
 package com.pluralsight;
 
+import com.pluralsight.objects.Order;
+
+import java.util.Scanner;
+
 
 public class BowlAndBarApp {
+
+
+    private Scanner scanner;
+    private Order currentOrder;
+
+    private static final String[] PROTEINS = {
+            "Peanut Butter", "Almond Butter", "Protein Powder", "Hemp Seeds", "Chia Seeds", "Greek Yogurt"
+    };
+
+    private static final String[] PREMIUM_TOPPINGS = {
+            "Acai", "Pitaya", "Matcha", "Spirulina"
+    };
+
+    private static final String[] REGULAR_TOPPINGS = {
+            "Apple", "Banana", "Strawberry", "Blueberry", "Mango", "Pineapple", "Granola","Coconuts Flakes","Honey","Cacao Nibs"
+    };
+    private static final String[] CONDIMENTS = {
+            "Agave Syrup", "Maple Syrup", "Cinnamon", "Vanilla extract", "Coco powder", "Mint"
+
+    };
     public static void main(String[] args) {
+
 
     }
 }
