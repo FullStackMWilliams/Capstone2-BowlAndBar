d1f6dc6fe0a4f3d9cde0c63c4714c2d178d0c73f
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-11 21:03:03 -0500

    ʢ´•ᴥ•`ʡ Added home screen

--- BowlAndBar/src/main/java/com/pluralsight/BowlAndBarApp.java
+++ BowlAndBar/src/main/java/com/pluralsight/BowlAndBarApp.java
@@ -22,12 +22,42 @@ public class BowlAndBarApp {
     private static final String[] REGULAR_TOPPINGS = {
             "Apple", "Banana", "Strawberry", "Blueberry", "Mango", "Pineapple", "Granola","Coconuts Flakes","Honey","Cacao Nibs"
     };
+
     private static final String[] CONDIMENTS = {
             "Agave Syrup", "Maple Syrup", "Cinnamon", "Vanilla extract", "Coco powder", "Mint"
 
     };
+
+    public BowlAndBarApp() {
+        scanner = new Scanner(System.in);
+        currentOrder = null;
+    }
+
+
     public static void main(String[] args) {
+        BowlAndBarApp app = new BowlAndBarApp();
+        app.run();
+    }
+
+    public void run() {
+
+        System.out.println("\n======================================");
+        System.out.println("|        WELCOME TO BOWL & BAR!        |");
+        System.out.println("|    Fresh Smoothies, Juices & Bowls   |");
+        System.out.println("|                                      |");
+        System.out.println("========================================");
+
+        boolean running = true;
 
 
     }
+
+    private void displayHomeScreen() {
+        System.out.println("\n======================================");
+        System.out.println("HOME SCREEN");
+        System.out.println();
+
+    }
 }
+
+
